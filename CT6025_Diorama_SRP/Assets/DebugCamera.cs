07b0d60b1c434d843f38eaf18a4b1c53f1f8694f
unknown <s4010770@glos.ac.uk>
2023-01-08 23:35:27 +0000

    changes

--- CT6025_Diorama_SRP/Assets/DebugCamera.cs
+++ CT6025_Diorama_SRP/Assets/DebugCamera.cs
@@ -1,24 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DebugCamera : MonoBehaviour
 {
-	public float movementSpeed = 5.0f; // Adjust this value to control the camera's movement speed
+	public float movementSpeed = 10.0f; 
+	public float rotationSpeed = 10.0f; 
+	private bool lightingOn = true;
+
+	[SerializeField] private Light _directionalLight;
+
+	private void Awake()
+	{
+		_directionalLight.enabled = false;
+	}
 
 	private void Update()
 	{
-		// Check if the user is holding down the left mouse button
 		if (Input.GetMouseButton(1))
 		{
-			// Get the mouse delta (difference between the current and previous mouse position)
+			// Difference between the current and previous mouse position)
 			float mouseDeltaX = Input.GetAxis("Mouse X");
 			float mouseDeltaY = Input.GetAxis("Mouse Y");
 			
-			// Invert the mouse delta Y
+			// Invert vertical movement 
 			mouseDeltaY = -mouseDeltaY;
 
-			// Rotate the camera based on the mouse delta
 			transform.Rotate(Vector3.up, mouseDeltaX, Space.World);
 			transform.Rotate(Vector3.right, mouseDeltaY, Space.Self);
 		}
@@ -26,23 +31,48 @@ public class DebugCamera : MonoBehaviour
 		// Check if the user is pressing the WASD keys
 		if (Input.GetKey(KeyCode.W))
 		{
-			// Move the camera forward
 			transform.position += transform.forward * movementSpeed * Time.deltaTime;
 		}
 		if (Input.GetKey(KeyCode.S))
 		{
-			// Move the camera backward
 			transform.position -= transform.forward * movementSpeed * Time.deltaTime;
 		}
 		if (Input.GetKey(KeyCode.A))
 		{
-			// Move the camera left
 			transform.position -= transform.right * movementSpeed * Time.deltaTime;
 		}
 		if (Input.GetKey(KeyCode.D))
 		{
-			// Move the camera right
 			transform.position += transform.right * movementSpeed * Time.deltaTime;
 		}
+	}
+	
+	private void OnGUI()
+	{
+		Rect windowRect = new Rect(10, 10, 200, 200);
+		GUI.Window(0, windowRect, DrawWindow, "Camera Settings");
+	}
+
+	private Color color;
+	private void DrawWindow(int windowID)
+	{
+		// Movement speed
+		movementSpeed = GUI.HorizontalSlider(new Rect(10, 30, 180, 30), movementSpeed, 0.0f, 20.0f);
+		GUI.Label(new Rect(10, 10, 180, 20), "Movement Speed: " + movementSpeed.ToString("F2"));
+
+		// Rotation speed
+		rotationSpeed = GUI.HorizontalSlider(new Rect(10, 70, 180, 30), rotationSpeed, 0.0f, 20.0f);
+		GUI.Label(new Rect(10, 50, 180, 20), "Rotation Speed: " + rotationSpeed.ToString("F2"));
+		
+		if (GUI.Toggle(new Rect(10, 110, 180, 20), lightingOn, "Art Lighting") != lightingOn)
+		{
+			lightingOn = !lightingOn;
+			UpdateLighting();
+		}
+	}
+	
+	private void UpdateLighting()
+	{
+		_directionalLight.enabled = !lightingOn;
 	}
 } 
